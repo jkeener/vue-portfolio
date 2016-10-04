0c0165497b6390b9201baedcc3199f082b6e421c
James Keener <jimmybobkeenmachine@gmail.com>
2016-10-03 18:21:36 -0600

    Resume
    Made resume pages easier to update

--- components/Resume.vue
+++ components/Resume.vue
@@ -7,28 +7,38 @@
         <div class="1/1 grid__cell">
           <h2>Solo Exhibitions</h2>
         </div>
-        <p class="1/1 grid__cell">Date<span>Exhibition info</span></p>
-        <p class="1/1 grid__cell">Date<span>Exhibition info</span></p>
-        <p class="1/1 grid__cell">Date<span>Exhibition info</span></p>
-        <p class="1/1 grid__cell">Date<span>Exhibition info</span></p>
-        <p class="1/1 grid__cell">Date<span>Exhibition info</span></p>
-        <p class="1/1 grid__cell">Date<span>Exhibition info</span></p>
-        <p class="1/1 grid__cell">Date<span>Exhibition info</span></p>
+        <p class="1/1 grid__cell" v-for="show in solo">{{show.date}}<span>{{show.name}}</span></p>
         <div class="1/1 grid__cell">
           <h2>Group Exhibitions</h2>
         </div>
-        <p class="1/1 grid__cell">Date<span>Exhibition info</span></p>
-        <p class="1/1 grid__cell">Date<span>Exhibition info</span></p>
-        <p class="1/1 grid__cell">Date<span>Exhibition info</span></p>
-        <p class="1/1 grid__cell">Date<span>Exhibition info</span></p>
-        <p class="1/1 grid__cell">Date<span>Exhibition info</span></p>
-        <p class="1/1 grid__cell">Date<span>Exhibition info</span></p>
+        <p class="1/1 grid__cell" v-for="show in group">{{show.date}}<span>{{show.name}}</span></p>
   </section>
 </div>
 </template>
 
 <script>
-
+export default {
+  data () {
+    return {
+        solo: [
+            //Add your solo exibitions here
+            {date: '1-1-2016',name: 'Solo exhibition one'},
+            {date: '1-2-2016',name: 'Solo exhibition two'},
+            {date: '1-3-2016',name: 'Solo exhibition three'},
+            {date: '1-4-2016',name: 'Solo exhibition four'},
+            {date: '1-5-2016',name: 'Solo exhibition five'}
+        ],
+        group: [
+            //Add your group exibitions here
+            {date: '1-1-2016',name: 'Group exhibition one'},
+            {date: '1-2-2016',name: 'Group exhibition two'},
+            {date: '1-3-2016',name: 'Group exhibition three'},
+            {date: '1-4-2016',name: 'Group exhibition four'},
+            {date: '1-5-2016',name: 'Group exhibition five'}
+        ],
+    };
+  },
+}
 </script>
 
 <style lang="sass" scoped>
